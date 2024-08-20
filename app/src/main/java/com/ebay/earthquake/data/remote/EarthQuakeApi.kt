1f6c4a71fc8e028b768632d1699cc54157e9f911
shaslam <shaslam@ebay.com>
2024-08-20 07:41:59 -0400

    Removed support for custom start and end date to fetch list of Earthquakes.

--- app/src/main/java/com/ebay/earthquake/data/remote/EarthQuakeApi.kt
+++ app/src/main/java/com/ebay/earthquake/data/remote/EarthQuakeApi.kt
@@ -2,12 +2,8 @@ package com.ebay.earthquake.data.remote
 
 import com.ebay.earthquake.data.remote.response.EarthQuakesResponse
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface EarthQuakeApi {
     @GET("earthquakes")
-    suspend fun getEarthquakes(
-        @Query("starttime") startTime: String = "2024-01-01",
-        @Query("endtime") endTime: String = "2024-01-02"
-    ): EarthQuakesResponse
+    suspend fun getEarthquakes(): EarthQuakesResponse
 }
