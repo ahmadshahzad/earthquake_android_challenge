76c41bccb60ef61982b991720c21caeb7732d79b
Yasin Tanriverdi <ytanriverdi@ebay.com>
2024-08-21 15:07:59 +0200

    startTime and endTime query parameters are added for specific day

--- app/src/main/java/com/ebay/earthquake/data/remote/EarthQuakeApi.kt
+++ app/src/main/java/com/ebay/earthquake/data/remote/EarthQuakeApi.kt
@@ -2,8 +2,12 @@ package com.ebay.earthquake.data.remote
 
 import com.ebay.earthquake.data.remote.response.EarthQuakesResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface EarthQuakeApi {
     @GET("earthquakes")
-    suspend fun getEarthquakes(): EarthQuakesResponse
+    suspend fun getEarthquakes(
+        @Query("starttime") startTime: String = "2024-01-03",
+        @Query("endtime") endTime: String = "2024-01-04"
+    ): EarthQuakesResponse
 }
